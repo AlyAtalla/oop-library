f1d8ff5af57c9ef781de431ca488cef63b958213
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-10-01 20:20:30 +0300

    Add student and teacher classes

---
+++ person.rb
@@ -0,0 +1,32 @@
+require './corrector'
+require_relative './rental'
+
+class Person
+  attr_accessor :name, :age
+  attr_reader :id, :rentals
+
+  def initialize(age:, name: 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @corrector = Corrector.new
+    @name = validate_name(name)
+    @age = age
+    @parent_permission = parent_permission
+    @rentals = []
+  end
+
+  def is_of_age?
+    @age >= 18
+  end
+
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
+
+  def validate_name(name)
+    @corrector.correct_name(name)
+  end
+
+  def add_rental(book, date)
+    Rental.new(date, self, book)
+  end
+end
