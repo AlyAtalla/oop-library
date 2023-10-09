3eace94b4f9dd45f9e7d056ab3424f4e41ab4aa7
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-10-09 11:42:22 +0300

    Refactor main.rb

--- main.rb
+++ main.rb
@@ -1,52 +1,65 @@
-require './app'
+require_relative 'app'
 
-def display_options
-  'Please choose an option by entering a number:
-  1 - List of all books
-  2 - List of all people
-  3 - Add a person
-  4 - Create a book
-  5 - Add a rental
-  6 - List of all rental for given id
-  7 - Exit'
-end
+class LibraryApp
+  def initialize
+    @app = App.new
+  end
 
-def user_option
-  user_choice = gets.chomp.to_i
-  user_choice.positive? && user_choice <= 7 ? user_choice : 'Invalid'
-end
+  def run
+    puts "Welcome to School library App!\n\n"
 
-def available_options(user_choice, library)
-  case user_choice
-  when 1
-    library.list_books
-  when 2
-    library.list_people
-  when 3
-    library.create_person
-  when 4
-    library.create_book
-  when 5
-    library.create_rental
-  when 6
-    library.list_rentals
-  else
-    puts 'Thank you for using this app!\\n'
+    loop do
+      display_options
+      print 'Your option: '
+      option = user_option
+
+      case option
+      when 1
+        @app.list_books
+      when 2
+        @app.list_people
+      when 3
+        @app.create_person
+      when 4
+        @app.create_book
+      when 5
+        @app.create_rental
+      when 6
+        print 'Enter person ID: '
+        person_id = gets.chomp.to_i
+        @app.list_rentals(person_id)
+      when 7
+        exit_app
+      else
+        puts 'Invalid option, please type a correct number!'
+      end
+    end
+  end
+
+  private
+
+  def display_options
+    puts <<~OPTIONS
+      Please choose an option by entering a number:
+      1 - List of all books
+      2 - List of all people
+      3 - Add a person
+      4 - Create a book
+      5 - Add a rental
+      6 - List of all rental for given id
+      7 - Exit
+    OPTIONS
+  end
+
+  def user_option
+    user_choice = gets.chomp.to_i
+    user_choice.between?(1, 7) ? user_choice : 'Invalid'
+  end
+
+  def exit_app
+    puts 'Thank you for using this app!'
     exit
   end
 end
 
-def main
-  library = App.new
-
-  puts "Welcome to School library App!\n\n"
-
-  loop do
-    puts display_options
-    print 'Your option: '
-    option = user_option
-    puts option == 'Invalid' ? 'Invalid option, please type correct number!' : available_options(option, library)
-  end
-end
-
-main
+LibraryApp.new.run
